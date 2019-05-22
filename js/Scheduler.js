9b0bc871b23b2b22a71700cf7190eb1fc0be47e7
Thumula Perera <thumulaperera@gmail.com>
2019-05-22 07:28:52 +0530

    final touches

--- js/Scheduler.js
+++ js/Scheduler.js
@@ -18,6 +18,7 @@ class Scheduler{
     }
 
     schedule(){
+        //creating an empty process for when the cpu is idle
         var empty_process = new Process("", 0, 0);
         empty_process.setColor("#FFFFFF");
 
@@ -25,6 +26,7 @@ class Scheduler{
 
         while ((!this._unreceived_jobs.isEmpty() || !this._schedule_queue.isEmpty())){
             
+            //add the processes to the schedule queue at their correct arrival times
             while(!this._unreceived_jobs.isEmpty()){
 
                 var process = this._unreceived_jobs.dequeue().element;
@@ -36,27 +38,21 @@ class Scheduler{
                 }
             }
 
-            console.log("before");
-            console.log(this._schedule_queue);
+            //maintaining an array to store processes that will be in the ready queue at each cpu burst time
             var temp_ready_queue = [];
             while(!this._schedule_queue.isEmpty()){
                 var ready_process = this._schedule_queue.dequeue().element;
                 temp_ready_queue.push(ready_process);
-            }
-            
+            }            
             for(var i=0; i < temp_ready_queue.length; i++){
                 this._schedule_queue.enqueue(temp_ready_queue[i], temp_ready_queue[i].getRemainingTime());
             }
-
             temp_ready_queue.shift();
             this._ready_queue[time] = temp_ready_queue;
-            console.log("after");
-            console.log(this._schedule_queue);
-
-            
+             
+            //execute the process with the lowest remaining time
             if(!this._schedule_queue.isEmpty()){
                 var current_process = this._schedule_queue.dequeue().element;
-                console.log((time + 1) + " " + current_process.getName());
                 this._executing_order[time] = current_process;
                 current_process.setRemainingTime(current_process.getRemainingTime() - 1);
                 if (current_process.getRemainingTime() > 0){
@@ -67,7 +63,6 @@ class Scheduler{
                 }
             } else {
                 this._executing_order[time] = empty_process;
-                console.log((time + 1) + " no processes");
             }
             time += 1;
         }
@@ -84,7 +79,7 @@ class Scheduler{
             var process = this._finished_jobs[i];
             total_turnaround_time += this.calcTurnaroundTime(process);
         }
-        return Math.round(total_turnaround_time * 1000/total_no_of_processes) / 1000;
+        return Math.round(total_turnaround_time * 100/total_no_of_processes) / 100;
     }
 
     calcWaitingTime(process){
@@ -98,7 +93,7 @@ class Scheduler{
             var process = this._finished_jobs[i];
             total_waiting_time += this.calcWaitingTime(process);
         }
-        return Math.round(total_waiting_time * 1000/total_no_of_processes) / 1000;
+        return Math.round(total_waiting_time * 100/total_no_of_processes) / 100;
     }
 
     print(){
